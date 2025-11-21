216baf9070df85ebb7c0c587500923f90131804a
Bowen XU <58252898+bowen-xu@users.noreply.github.com>
2025-11-20 22:35:18 -0500

    modulize

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,10 @@
+add_requires("pybind11", {system = false})
+
+
 target("smart_ref")
-    set_kind("headeronly")
-    add_headerfiles("includes/*.hpp")
-    add_headerfiles("includes/smart_ref/*.hpp", {prefixdir = "smart_ref"})
+    set_kind("static")
+    add_packages("pybind11")
+    add_files("include/**.mxx", {public = true})
+    -- add_includedirs("include", {public = true})
+    -- add_headerfiles("include/*.hpp")
+    -- add_headerfiles("include/smart_ref/*.hpp", {prefixdir = "smart_ref"})
